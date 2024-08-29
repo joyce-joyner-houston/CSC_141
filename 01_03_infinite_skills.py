93ba59d8408eafbe35ac38b2dea335ceedfc9dc0
Joyce Joyner-Houston <joycejoynerhouston@gmail.com>
2024-08-29 05:55:55 -0700

    Update 01_03_infinite_skills.py

--- 01_03_infinite_skills.py
+++ 01_03_infinite_skills.py
@@ -1,3 +1,13 @@
-...
+'''
 
-...
+Some skills that will help me in this course are:
+
+Being able to fix an errors within the code
+
+Becoming familiar with python
+
+Learning more about AI
+
+Using common tags like classes, id's, and integers
+
+'''
