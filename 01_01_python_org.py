61eec3014dfc93cab7f604a16b89055722f97fd8
Joyce Joyner-Houston <joycejoynerhouston@gmail.com>
2024-09-04 05:17:50 -0700

    Update 01_01_python_org.py

--- 01_01_python_org.py
+++ 01_01_python_org.py
@@ -1,7 +1,15 @@
-#Having comments makes errors easier to fix
+#Having comments makes errors easier to fix.
 
 #Commands are read in interactive mode.
 
-#Primary prompt uses (>>>) while secondary prompts use (...)
+#Primary prompt uses (>>>) while secondary prompts use (...).
 
-#Integer numbers use (int) while fractions use (float)
+#Integer numbers use (int) while fractions use (float).
+
+#Class tags need to be activated using the "class" tag or have a variable using "MyClass".
+
+#Making looping dictionaries is almost the same as using items().
+
+#You can make a loop for more than two sequences by using the zip() function.
+
+#The range function is used to generate arithmetic progressions.
