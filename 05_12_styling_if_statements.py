4c0770e4530db31e8a0b86c306cc3b419209509e
Joyce Joyner-Houston <joycejoynerhouston@gmail.com>
2024-10-02 10:15:58 -0400

    Add files via upload

---
+++ 05_12_styling_if_statements.py
@@ -0,0 +1,18 @@
+#Test of usernames
+usernames = ['admin', 'James', 'Emily', 'Tommy', 'Karen']
+
+#Testing loops
+if 'admin' in usernames:
+    print("Hello admin, would you like to a see a status report")
+
+elif 'James' in usernames:
+    print("Hello James, thank you for logging in again")
+
+elif 'Emily' in usernames:
+    print("Hello Emily, thank you for logging in again")
+
+elif 'Tommy' in usernames:
+    print("Hello Tommy, thank you for logging in again")
+
+elif 'Karen' in usernames:
+    print("Hello Karen, thank you for logging in again")
---
+++ 05_13_your_ideas.py
@@ -0,0 +1,7 @@
+#I want to create a good straegy base game
+
+#The game will be a card game
+
+#The game will require the player to think fast as the levels get harder
+
+#The game will also be survival based
